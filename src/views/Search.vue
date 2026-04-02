28034979ab46d677ded7ea03785d98242f7644f5
shermyliou <shermy.liou@gmail.com>
2026-04-02 15:15:01 +0800

    adjust layout for checking search result

---
+++ src/views/Search.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+  <div>
+    <input type="checkbox" class="btn-check" id="btn-check" autocomplete="off">
+    <label class="btn btn-outline-secondary btn-rect btn-sm" for="btn-check">Single toggle</label>
+
+    <input type="checkbox" class="btn-check" id="btn-check-2" checked autocomplete="off">
+    <label class="btn btn-outline-secondary btn-rect btn-sm" for="btn-check-2">Checked</label>
+
+    <input type="checkbox" class="btn-check" id="btn-check-3" autocomplete="off" disabled>
+    <label class="btn btn-outline-secondary btn-rect btn-sm" for="btn-check-3">Disabled</label>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  
+</style>
