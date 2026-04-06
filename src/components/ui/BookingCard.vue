c309a590501d1f8984eae99705b55978cb693cf6
metaphor1212 <metaphor@gmail.com>
2026-04-07 04:36:00 +0800

    改按鈕

---
+++ src/components/ui/BookingCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-white border border-[#cdcdcd] flex flex-col p-4 md:p-8 relative rounded-3xl w-full shadow-sm">
+    <div class="flex flex-col gap-4 w-full">
+      <div class="space-y-2">
+        <h3 class="text-[20px] font-normal text-[#212529] leading-tight">
+          2026/3/28 (六) 10:00
+        </h3>
+        <p class="text-[20px] font-normal text-[#212529] leading-tight">
+          衛武營國家藝術文化中心
+        </p>
+      </div>
+
+      <div class="text-[16px] text-black space-y-1">
+        <p>剩：253</p>
+        <p>票價：750、1,000</p>
+      </div>
+    </div>
+
+    <button class="btn btn-primary btn-rect">購買</button>
+    <!-- <button class="mt-6 bg-[#111] text-white py-4 px-6 rounded-lg w-full hover:bg-gray-800 transition-colors">
+      購買
+    </button> -->
+  </div>
+</template>
+
+<script setup>
+
+</script>
