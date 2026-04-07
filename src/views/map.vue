1c79104f8dbbce30ebafe427529e4180bbdabe1b
ShirlineWu <wyy2319@gmail.com>
2026-04-07 14:43:18 +0800

    add map

---
+++ src/views/map.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import BootstrapChecker from './BootstrapChecker.vue'
+</script>
+
+
+<template>
+    <div class="container">
+        <img src="../public/images/台北地區地圖-橘色道路1.jpg" alt="">
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+    
+</style>
