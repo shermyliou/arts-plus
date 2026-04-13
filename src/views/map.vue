13f71747c568c1ca9bb83e548cb0e7f0fe708295
ShirlineWu <256025004+ShirlineWu@users.noreply.github.com>
2026-04-13 09:33:11 +0800

    map圖片寬變更

--- src/views/map.vue
+++ src/views/map.vue
@@ -1,18 +1,15 @@
 <script setup>
-    import BootstrapChecker from './BootstrapChecker.vue'
+import BootstrapChecker from "./BootstrapChecker.vue";
 </script>
 
-
 <template>
-    <div class="container">
-        <img src="/images/mapHome.jpg" alt="" class="map-img">
-    </div>
+  <div class="container">
+    <img src="/images/mapHome.jpg" alt="" class="map-img" />
+  </div>
 </template>
 
-
-
 <style lang="scss" scoped>
-    .map-img{
-        height: 1080px;
-    }
+.map-img {
+  height: 1080px;
+}
 </style>
